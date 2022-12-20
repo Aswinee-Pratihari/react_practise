df4ac3c04363cec77238b1a92069ae2f99fb2502
Aswinee-Pratihari <aswineepratihari010@gmail.com>
2022-12-20 11:03:22 +0530

    refactor-Chapter5(click event)

--- src/Content.jsx
+++ src/Content.jsx
@@ -5,11 +5,27 @@ const Content = () => {
         return names[int];
     }
 
+
+    const handleClick=()=>{
+        console.log('you clicked it')
+        alert('you clicked it')
+    }
+    const handleClick2=(name)=>{
+        // console.log('you clicked it')
+        alert(`${name} clicked it`)
+    }
+    const handleClick3=(e)=>{
+        
+        console.log(e.target)
+    }
     return (
         <main>
-            <p>
+            <p onDoubleClick={handleClick}>
                 Hello {handleNameChange()}!
             </p>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={()=>handleClick2(handleNameChange())}>Get your name</button>
+            <button onClick={(e)=>handleClick3(e)}>Get the event object</button>
         </main>
     )
 }
